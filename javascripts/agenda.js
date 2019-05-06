b5e65a97fd44990698264b4da8d9638c411b8bf9
Lidia Cozmuta <maximlidia59@gmail.com>
2019-05-06 21:32:47 +0300

    delete + add new person

--- javascripts/agenda.js
+++ javascripts/agenda.js
@@ -1,26 +1,33 @@
 var allPersons = [];
 
 var API_URL = {
-   // ADD: 'data/add.json'
-   ADD: 'users/add'
+    // ADD: 'data/add.json'
+    ADD: 'users/add',
+    DELETE: 'users/delete'
 };
 var API_METHOD = {
     //ADD: 'GET'
-    ADD: 'POST'
+    ADD: 'POST',
+    DELETE: 'DELETE'
 }
 fetch('data/persons.json').then(function (r) {
     return r.json();
 }).then(function (persons) {
     console.log('all persons', persons);
+    allPersons = persons;
+    display(persons);
 })
 
-function diplay(persons) {
+function display(persons) {
     var list = persons.map(function (person) {
-        return `<tr>
-            <td>${person.fisrtName}</td>
+        return `<tr data-id="${person.id}">
+            <td>${person.firstName}</td>
             <td>${person.lastName}</td>
             <td>${person.phone}</td>
-            <td></td>
+            <td>
+                <a href="#" class="delete">&#10006;</a>
+                <a href="#" class="edit">&#9998;</a>
+            </td>
           </tr>`;
     });
 
@@ -40,13 +47,52 @@ function submitNewPerson(firstName, lastName, phone) {
             firstName: firstName,
             lastName: lastName,
             phone: phone
-        })  
+        });
     }
-      
-    console.warn('submitNewPerson', body);
-    
+
     fetch(API_URL.ADD, {
         method: API_METHOD.ADD,
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function (r) {
+        return r.json();
+    }).then(function (status) {
+        if (status.success) {
+            inlineAddPerson(status.id, firstName, lastName, phone);
+        } else {
+            console.warn('not save!', status);
+        }
+    })
+
+}
+
+function inlineAddPerson(id, firstName, lastName, phone) {
+    allPersons.push({
+        id,
+        firstName,
+        lastName,
+        phone
+    });
+    display(allPersons);
+}
+
+function inlineDeletePerson(id) {
+    console.warn('please refresh :)', id);
+    allPersons = allPersons.filter(function(person) {
+        return person.id != id;
+    });
+    display(allPersons);
+}
+
+function deletePerson(id) {
+    var body = null;
+    if (API_METHOD.DELETE === 'DELETE') {
+        body = JSON.stringify({ id });
+    }
+    fetch(API_URL.DELETE, {
+        method: API_METHOD.DELETE,
         body: body,
         headers: {
             "Content-Type": "application/json"
@@ -54,24 +100,23 @@ function submitNewPerson(firstName, lastName, phone) {
     }).then(function(r) {
         return r.json();
     }).then(function(status) {
-        if(status.success) {
-            inlineAddPerson(firstName, lastName, phone);
+        if (status.success) {
+            inlineDeletePerson(id);
         } else {
-            console.warn('not save!', status);
+            console.warn('not removed!', status);
         }
     })
-
-    }
-
-function inlineAddPerson(firstName, lastName, phone) {
-    allPersons.push({
-        firstName: firstName,
-        lastName: lastName,
-        phone: phone
+}
+function initEvents() {
+    const tbody = document.querySelector('#agenda tbody');
+    tbody.addEventListener('click', function(e) {
+        if (e.target.className == 'delete') {
+            const tr = e.target.parentNode.parentNode;
+            const id = tr.getAttribute('data-id');
+            deletePerson(id);
+        }
     });
-    display(allPersons);
 }
 
-
-
+initEvents();
 
